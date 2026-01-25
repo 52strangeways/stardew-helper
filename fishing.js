819f8aec10924d56587ad2c691bf22663d5a1ef7
52strangeways <goodnightcuriosity@gmail.com>
2026-01-25 21:53:50 +0800

    Update fishing.js

--- fishing.js
+++ fishing.js
@@ -90,11 +90,9 @@ function renderTable() {
     const isAngler = document.getElementById('anglerToggle')?.checked;
     const filter = document.getElementById('fishSearch')?.value.toLowerCase() || '';
     
-    const fishes = sourceData.filter(f => 
-    (f.name && f.name.includes(filter)) || 
-    (f.en && f.en.toLowerCase().includes(filter)) || 
-    (f.loc && (f.loc || '').includes(filter)) // 加上安全保護
-);
+    const fishes = fishData[currentSeason].filter(f => 
+        f.name.includes(filter) || f.en.toLowerCase().includes(filter) || f.loc.includes(filter)
+    );
 
 
     root.innerHTML = `
